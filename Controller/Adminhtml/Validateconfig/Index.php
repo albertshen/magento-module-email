df5cb5eada9514b67ed2d7f321b9a4177b954233
Albert Shen <albertshen1206@gmail.com>
2022-01-26 01:23:56 +0800

    up

--- Controller/Adminhtml/Validateconfig/Index.php
+++ Controller/Adminhtml/Validateconfig/Index.php
@@ -14,7 +14,7 @@ class Index extends Action
     /**
      * Authorization level of a basic admin session
      */
-    const ADMIN_RESOURCE = 'AlbertMage_Email::config';
+    const ADMIN_RESOURCE = 'AlbertMage_Email::albertmage_email';
     
     /**
      * Index action
